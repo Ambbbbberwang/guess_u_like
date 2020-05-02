18a71b3b2483953b005cb7304d770f45059a7746
Lisa Combs <eac721@nyu.edu>
2020-05-02 12:42:46 -0400

    import spark context

--- recommender.py
+++ recommender.py
@@ -83,6 +83,8 @@ def data_prep(spark, spark_df, pq_path='hdfs:/user/eac721/onepct_int.parquet', f
 
 def rand_samp(df):
 
+    from pyspark.sql import SQLContext
+
     users = df.select('user_id').distinct()
 
     test = sqlContext.createDataFrame(sc.emptyRDD(), df.schema)
