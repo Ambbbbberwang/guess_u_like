cd85b6b426b1a6d3920de73c3b875dfdd6bebae3
Xiangyun Chu <xc1511@nyu.edu>
2020-05-06 02:40:43 -0400

    Add Run Time

--- recsys.py
+++ recsys.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+
 # rec_fit & rec_test & ranking_evaluator
 
 from pyspark.ml.recommendation import ALS #, Rating
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql import functions as f
-from pyspark.sql.types import DoubleType
-import numpy as np
+#from pyspark.sql.types import DoubleType
+#import numpy as np
+import timeit
+from pyspark.sql import Window
+from pyspark.mllib.evaluation import RankingMetrics
 
 
 
@@ -13,6 +18,8 @@ import numpy as np
 def RecSys_fit (train, val, metric = 'RMSE', seed = 42,ranks = [10, 15], 
             regParams = [0.01, 0.15], maxIters = [10]):
     
+    # Record Start Time
+    fit_start=timeit.default_timer()
 
     # Build the recommendation model using ALS on the training data
     als = ALS(userCol="user_id", itemCol="book_id", ratingCol="rating",
@@ -24,7 +31,7 @@ def RecSys_fit (train, val, metric = 'RMSE', seed = 42,ranks = [10, 15],
         # Initialize best_score to track min
         best_score = float('inf')
     
-    elif metric in ['Precision','MAP','NDCG']:   # Are we doing MAP or other ranking metric???
+    elif metric in ['Precision','MAP','NDCG']:
         
         #Initialize best_score to track max
         best_score = -1*float('inf')  
@@ -89,11 +96,15 @@ def RecSys_fit (train, val, metric = 'RMSE', seed = 42,ranks = [10, 15],
     print('The best model was trained with rank %s, regParam %s and maxIter %s' 
           % (ranks[best_params[0]], regParams[best_params[1]], maxIters[best_params[2]]))
     
-    print('The best model has %s of value %s:' % (metric, best_score))
+    print('The best model has %s of value: %s' % (metric, best_score))
     
     if best_model == None:
         print('Returned model is None. Error???')
         
+    # Record End Time & Print Run Time
+    fit_end = timeit.default_timer()
+    print('Fitting Run Time: ', fit_end - fit_start)
+    
     return best_model
         
 
@@ -104,43 +115,40 @@ def RecSys_fit (train, val, metric = 'RMSE', seed = 42,ranks = [10, 15],
 
 def RecSys_test(test, best_model):
     
-    # TO BE CONTINUE
+    # Record Start Time
+    test_start=timeit.default_timer()
     
-    predict_test_df = best_model.transform(test)
-    
+    predict_test_df = best_model.transform(test)  
     
     # Generate RMSE
     predict_test_df = predict_test_df.filter(predict_test_df.prediction != float('nan'))    
-    eval_RMSE = RegressionEvaluator(metricName="rmse", labelCol="rating", 
-                                    predictionCol="prediction")   
+    eval_RMSE = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")   
     test_RMSE = eval_RMSE.evaluate(predict_test_df)
     print('RMSE of Best Model on Test Set: ', test_RMSE,'\n')
     
     # Generate Precision at 500
     test_precision = Ranking_evaluator(best_model, test, 'Precision')
-    print('Precition at 500 of Best Model on Test Set: ', test_precision)
+    print('Precition at 500 of Best Model on Test Set: ', test_precision,'\n')
     
     # Generate MAP
     test_MAP = Ranking_evaluator(best_model, test, 'MAP')
-    print('MAP of Best Model on Test Set: ', test_MAP)
+    print('MAP of Best Model on Test Set: ', test_MAP,'\n')
     
     # Generate NDCG at 500
     test_NDCG = Ranking_evaluator(best_model, test, 'NDCG')
-    print('NDCG at 500 of Best Model on Test Set: ', test_NDCG)
+    print('NDCG at 500 of Best Model on Test Set: ', test_NDCG,'\n')
 
+    # Record End Time
+    test_end=timeit.default_timer()
+    print('Testing Run Time:', test_end - test_start)
     
     
 
 
 #-----------------Ranking Evaluator----------------------------
 
-from pyspark.sql.functions import *
-from pyspark.sql import Window
-from pyspark.mllib.evaluation import RankingMetrics
-    
-    
-    
-def Ranking_evaluator (model, val, metric_type):
+     
+def Ranking_evaluator (spark, model, val, metric_type):
     
     
     val.createOrReplaceTempView('val')
@@ -148,14 +156,14 @@ def Ranking_evaluator (model, val, metric_type):
     val_rec = model.recommendForUserSubset(val_user,500)
     #val_rec.printSchema()
     
-    val_rec = val_rec.select('user_id','recommendations',posexplode('recommendations')).drop('pos').drop('recommendations')
+    val_rec = val_rec.select('user_id','recommendations',f.posexplode('recommendations')).drop('pos').drop('recommendations')
     val_rec = val_rec.select('user_id',f.expr('col.book_id'),f.expr('col.rating'))
     
     w= Window.partitionBy('user_id')
-    val_recrank=val_rec.select('user_id',collect_list('book_id').over(w).alias('rec_rank')).sort('user_id').distinct()
+    val_recrank=val_rec.select('user_id',f.collect_list('book_id').over(w).alias('rec_rank')).sort('user_id').distinct()
    
-    val = val.sort(desc('rating'))
-    val_truerank=val.select('user_id', collect_list('book_id').over(w).alias('true_rank')).sort('user_id').distinct()
+    val = val.sort(f.desc('rating'))
+    val_truerank=val.select('user_id', f.collect_list('book_id').over(w).alias('true_rank')).sort('user_id').distinct()
     
     scoreAndLabels = val_recrank.join(val_truerank,on=['user_id'],how='inner')
     
