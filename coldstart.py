718ef9f073f67c754a6adba5f781e1b86993821d
Ambbbbberwang <yw2115@nyu.edu>
2020-05-06 23:57:36 -0500

    wrap up in function attribute_matrix

--- coldstart.py
+++ coldstart.py
@@ -20,61 +20,66 @@ genre_df =spark.read.json('hdfs:/user/yw2115/gooreads_book_genres_initial.json.g
 
 
 
+def build_attribute_matrix(book_df,author_df,genre_df):
+
+    ####Create Attribute Matrix for Genres####
+    '''
+    10 categories: 
+    children| comics, graphic| fantasy, paranormal| fiction| history, historical fiction, biography,
+    mystery, thriller, crime| non-fiction| poetry| romance| young-adult
+
+    '''
+    import pyspark.sql.functions as f
+    from pyspark.sql.functions import when
+
+    genre_at = genre_df.select('book_id',f.expr('genres.children'),f.expr('genres.`comics, graphic`'),\
+        f.expr('genres.`fantasy, paranormal`'),f.expr('genres.fiction'), \
+        f.expr('genres.`history, historical fiction, biography`'), f.expr('genres.`mystery, thriller, crime`'),\
+        f.expr('genres.`non-fiction`'),f.expr('genres.poetry'),f.expr('genres.romance'),f.expr('genres.`young-adult`'))
 
 
-####Create Attribute Matrix for Genres####
-'''
-10 categories: 
-children| comics, graphic| fantasy, paranormal| fiction| history, historical fiction, biography,
-mystery, thriller, crime| non-fiction| poetry| romance| young-adult
+    #change col names
+    new_col = ['book_id','g1','g2','g3','g4','g5','g6','g7','g8','g9','g10']
+    genre_at = genre_at.toDF(*new_col)
 
-'''
-import pyspark.sql.functions as f
-from pyspark.sql.functions import when
+    #0/1 Encoding
+    #change Null value to 0 (meaning the book is not in this genre) 
+    # and other int to 1 (meaning the book in this genre)
 
-genre_at = genre_df.select('book_id',f.expr('genres.children'),f.expr('genres.`comics, graphic`'),\
-    f.expr('genres.`fantasy, paranormal`'),f.expr('genres.fiction'), \
-    f.expr('genres.`history, historical fiction, biography`'), f.expr('genres.`mystery, thriller, crime`'),\
-    f.expr('genres.`non-fiction`'),f.expr('genres.poetry'),f.expr('genres.romance'),f.expr('genres.`young-adult`'))
+    for i in range(1,len(new_col)):
+        col_name = new_col[i]
+        genre_at = genre_at.withColumn(col_name, when(genre_at[col_name].isNotNull(), 1).otherwise(0))
 
+    #genre_at.show(10)
 
-#change col names
-new_col = ['book_id','g1','g2','g3','g4','g5','g6','g7','g8','g9','g10']
-genre_at = genre_at.toDF(*new_col)
-
-#0/1 Encoding
-#change Null value to 0 (meaning the book is not in this genre) 
-# and other int to 1 (meaning the book in this genre)
-
-for i in range(1,len(new_col)):
-    col_name = new_col[i]
-    genre_at = genre_at.withColumn(col_name, when(genre_at[col_name].isNotNull(), 1).otherwise(0))
-
-#genre_at.show(10)
-
-####Add Author Rating as Additional Attribute####
-#Select the first author (there are books with more than 1 author, first author is the main author)
-book_df = book_df.select('book_id',f.expr('authors[0]').alias('a'))
-#Add author_id
-book_df = book_df.select('book_id',f.expr('a.author_id'))
+    ####Add Author Rating as Additional Attribute####
+    #Select the first author (there are books with more than 1 author, first author is the main author)
+    book_df = book_df.select('book_id',f.expr('authors[0]').alias('a'))
+    #Add author_id
+    book_df = book_df.select('book_id',f.expr('a.author_id'))
 
 
-#Join book_df and author_df
-book_df.createOrReplaceTempView('book_df')
-author_df.createOrReplaceTempView('author_df')
+    #Join book_df and author_df
+    book_df.createOrReplaceTempView('book_df')
+    author_df.createOrReplaceTempView('author_df')
 
-author_at = spark.sql('SELECT book_df.book_id, book_df.author_id,\
- author_df.average_rating FROM book_df JOIN author_df ON \
- book_df.author_id=author_df.author_id')
-#author_at.show(10)
+    author_at = spark.sql('SELECT book_df.book_id, book_df.author_id,\
+     author_df.average_rating FROM book_df JOIN author_df ON \
+     book_df.author_id=author_df.author_id')
+    #author_at.show(10)
 
-####Join The Two Matrix to Get Book Attribute Matrix####
-genre_at.createOrReplaceTempView('genre_at')
-author_at.createOrReplaceTempView('author_at')
+    ####Join The Two Matrix to Get Book Attribute Matrix####
+    genre_at.createOrReplaceTempView('genre_at')
+    author_at.createOrReplaceTempView('author_at')
 
-book_at = spark.sql('SELECT genre_at.book_id, genre_at.g1, genre_at.g2,\
- genre_at.g3, genre_at.g4, genre_at.g5, genre_at.g6, genre_at.g7, genre_at.g8, \
- genre_at.g9, genre_at.g10, author_at.average_rating AS author_rating \
- FROM genre_at JOIN author_at ON genre_at.book_id=author_at.book_id')
+    book_at = spark.sql('SELECT genre_at.book_id, genre_at.g1, genre_at.g2,\
+     genre_at.g3, genre_at.g4, genre_at.g5, genre_at.g6, genre_at.g7, genre_at.g8, \
+     genre_at.g9, genre_at.g10, author_at.average_rating AS author_rating \
+     FROM genre_at JOIN author_at ON genre_at.book_id=author_at.book_id')
+
+    #return the I*N attribute matrix for book
+    #I is number of items (books)
+    #N = 11 is number of attribute features of the books
+    return book_at
 
 
